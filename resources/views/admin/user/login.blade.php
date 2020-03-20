608447e8a02367e69ce26abdd39107700888c923
Đỗ Quang Đức <ducgets@gmail.com>
2020-03-21 02:16:02 +0700

    duc edit

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -6,34 +6,28 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="icon" type="img/png" href="img/icons/favicon.ico" />
+<link rel="icon" type="img/png" href="img/icons/favicon.png" />
 
-<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet"  href="<?php echo asset('css/bootstrap.min.css')?>">
 
-<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-
-<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-
-<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-
-<link rel="stylesheet" type="text/css" href="css/util.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-
+<link rel="stylesheet" type="text/css" href="css/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../../public/css/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="css/fonts/iconic/css/material-design-iconic-font.min.css">
+    <link href="{{ URL::asset('css/css-hamburgers/hamburgers.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo asset('css/animate/animate.css')?>">
+<link rel="stylesheet"  href="<?php echo asset('css/css-hamburgers/hamburgers.css')?>">
+<link rel="stylesheet"  href="<?php echo asset('css/animsition/css/animsition.css/animsition.min.css')?>">
+<link rel="stylesheet"  href="<?php echo asset('css/select2/select2.min.css')?>">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/daterangepicker/daterangepicker.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}" >
+<link rel="stylesheet"  href="<?php echo asset('css/mainlogin.css')?>">
 </head>
 <body>
 <div class="container-login100" style="background-image: url('img/bg-01.jpg');">
 <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
 <form class="login100-form validate-form">
 <span class="login100-form-title p-b-37">
-Đăng nhập 
+Đăng nhập
 </span>
 <div class="wrap-input100 validate-input m-b-20" data-validate="Nhập tên người dùng hoặc email">
 <input class="input100" type="text" name="Nhập tên người dùng" placeholder="Nhập tên người dùng hoặc email">
@@ -45,7 +39,7 @@
 </div>
 <div class="container-login100-form-btn">
 <button class="login100-form-btn">
-Đăng nhập 
+Đăng nhập
 </button>
 </div>
 <div class="text-center p-t-57 p-b-20">
@@ -71,16 +65,14 @@ Hoặc đăng nhập bằng
 </div>
 <div id="dropDownSelect1"></div>
 
-<script src="vendor/jquery/jquery-3.2.1.min.js" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/jquery/jquery-3.2.1.min.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/animsition/js/animsition.min.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/bootstrap/js/popper.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/bootstrap/js/bootstrap.min.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/select2/select2.min.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
+<script rc="{{ URL::asset('css/daterangepicker/moment.min.js') }}" type="8b56910566ba752a634f3984-text/javascript"></script>
 
-<script src="vendor/animsition/js/animsition.min.js" type="8b56910566ba752a634f3984-text/javascript"></script>
 
-<script src="vendor/bootstrap/js/popper.js" type="8b56910566ba752a634f3984-text/javascript"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js" type="8b56910566ba752a634f3984-text/javascript"></script>
-
-<script src="vendor/select2/select2.min.js" type="8b56910566ba752a634f3984-text/javascript"></script>
-
-<script src="vendor/daterangepicker/moment.min.js" type="8b56910566ba752a634f3984-text/javascript"></script>
 <script src="vendor/daterangepicker/daterangepicker.js" type="8b56910566ba752a634f3984-text/javascript"></script>
 
 <script src="vendor/countdowntime/countdowntime.js" type="8b56910566ba752a634f3984-text/javascript"></script>
