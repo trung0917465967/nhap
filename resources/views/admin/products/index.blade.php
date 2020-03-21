432edaf4c32ccf021efa04555a4a4421d1233cf8
trantrung <trantrung98nd@gmail.com>
2020-03-21 18:29:03 +0700

    trung add

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,22 +1,4 @@
-{{-- <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo asset('css/jsgrid/jsgrid-theme.min.css')?>">
-    <!-- Font Awesome -->
-    <link rel="stylesheet"  href="<?php echo asset('css/fontawesome-free/css/all.min.css')?>">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="<?php echo asset('css/datatables-bs4/css/dataTables.bootstrap4.css')?>">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo asset('css/dist/css/adminlte.min.css')?>">
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> --}}
+
     <style>
         .btnindex {
             background-color: DodgerBlue;
