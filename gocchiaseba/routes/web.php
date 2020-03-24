cdf3e6d4e17d38b157932cec4e8019629dfd83e4
Đỗ Quang Đức <ducgets@gmail.com>
2020-03-24 21:13:31 +0700

    add

--- gocchiaseba/routes/web.php
+++ gocchiaseba/routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('admin.index', ['as' => 'admin.index', 'uses' => 'UserController@index']);
+routeAlias::group(['prefix'=>'admin'],function() {
+    routeAlias::get('admin.register', [ 'as' => 'register', 'uses' => 'UserController@register']);
+  //    Route::post('store',[ 'as' => 'store', 'uses' =>  'UserController@store']);
+    routeAlias::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
+      routeAlias::get('admin.layout',[ 'as' => 'admin.layout', 'uses' => 'UserController@layout']);
+  //    route::post('login',[ 'as' => 'login', 'uses' => 'UserController@postlogin']);
+  //    Route::get('logout',[ 'as' => 'logout', 'uses' => 'UserController@logout']);
+  //    routeAlias::resource('user', 'userController');
+      routeAlias::get('user.create',[ 'as' => 'user.create', 'uses' => 'UserController@create']);
+      routeAlias::get('user.index',[ 'as' => 'user.index', 'uses' => 'UserController@index']);
+      routeAlias::resource('products', 'ProductController');
+  });
+  routeAlias::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
+   routeAlias::get('logout',[ 'as' => 'logout', 'uses' => 'UserController@logout']);
+   routeAlias::get('user.register',[ 'as'   => 'user.register', 'uses' => 'UserController@register']);
+   routeAlias::get('blog.index',[ 'as'   => 'blog.index', 'uses' => 'BlogController@index']);
+   routeAlias::get('products.create',[ 'as'   => 'products.create', 'uses' => 'ProductController@create']);
+   routeAlias::get('blog.add',[ 'as'   => 'blog.add', 'uses' => 'BlogController@add']);
